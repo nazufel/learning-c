e612fa74b55ef9da783e35d01f61f3c5a0ceccf0
nazufel <nazufel@gmail.com>
2021-12-01 22:40:27 -0500

    variables

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# learning-c
+# Learning C
+
+This repo is code for me learning C. Some of the resources
+
+* [Free Code Camp YouTube](https://www.youtube.com/watch?v=KJgsSFOSQv0)
---
+++ variables/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    // character ("string") variable
+    char characterName[] = "John";
+
+    // int variable 
+    int characterAge = 35;
+
+    printf("There once was a man named %s.\n", characterName);
+    printf("He was %d.\n", characterAge);
+    printf("He really liked the name %s.\n", characterName);
+    printf("but he did not like being %d.\n", characterAge);
+
+    return 0;
+}
