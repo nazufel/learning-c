262ce57f30d1d83f362c595628a3842b6a8bc3a1
nazufel <nazufel@gmail.com>
2021-12-01 22:42:39 -0500

    change var half way through.

--- variables/main.c
+++ variables/main.c
@@ -11,6 +11,10 @@ int main() {
 
     printf("There once was a man named %s.\n", characterName);
     printf("He was %d.\n", characterAge);
+
+    // change age variable
+    characterAge = 30;
+
     printf("He really liked the name %s.\n", characterName);
     printf("but he did not like being %d.\n", characterAge);
 
