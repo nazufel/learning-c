0dff8331f9b23d8454b87db99851f3e5ee805d90
nazufel <nazufel@gmail.com>
2021-12-01 22:50:46 -0500

    have basic data types

---
+++ dataTypes/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    // integer var
+    int age = 30;
+
+    // double var
+    double gpa = 3.7;
+    
+    // store a single character, must use single quotes
+    char grade = 'A';
+
+    // store a string of characters in an array, use double quotes
+    char class[] = "Compute Science";
+
+    return 0;
+}
