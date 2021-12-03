39b7884a9c8b74220c0ce800648d357ef09b38f0
nazufel <nazufel@gmail.com>
2021-12-03 11:15:49 -0500

    comments

---
+++ 06-comments/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    // single line comment
+
+    /*
+        multi line comment
+    */
+    printf("Comments are fun\n");
+
+    return 0;
+}
