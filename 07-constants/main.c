f542ec02d4c0651001e3414b0d988b03a1570ed5
nazufel <nazufel@gmail.com>
2021-12-03 11:20:38 -0500

    input

---
+++ 07-constants/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main() {
+
+
+    // a convention is to place constants as all uppercase
+    const int NUM = 5;
+
+    printf("%d\n", NUM);
+
+    return 0;
+}
---
+++ 08-userInput/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main() {
+
+    // print math operations on doubles
+    printf("%f\n", 8.9 + 4.5 );
+
+    // using mathmatical functios
+    printf("%f\n", pow(2,3) );
+
+    printf("%f\n", sqrt(36) );
+
+    printf("%f\n", ceil(36.234) );
+
+    printf("%f\n", floor(36.234) );
+
+    return 0;
+}
