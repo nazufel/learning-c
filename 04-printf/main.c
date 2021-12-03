2c25053de154c6ba4b473e5588b3d4518ff1c783
nazufel <nazufel@gmail.com>
2021-12-03 11:03:37 -0500

    printf

--- 04-printf/main.c
+++ 04-printf/main.c
@@ -3,7 +3,15 @@
 
 int main() {
 
-    printf("Hello World!\n");
+    int favNum = 90;
+    double favFloat = 500.5;
+
+    // print string and a number
+    printf("My favorite %s is: %d\n", "number", favNum);
+
+    // print a string and a float with format specifier
+    printf("My favorite %s is: %f\n", "float", favFloat);
+
 
     return 0;
 }
